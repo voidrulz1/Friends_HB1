e4e040766d6531720d413e8ecc4b574814a963ad
voidrulz1 <131723217+voidrulz1@users.noreply.github.com>
2023-04-29 13:48:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 require('dotenv').config();
-const Monitor = require('ping-monitor')
+//const Monitor = require('ping-monitor')
 const keepAlive = require('./server');
 keepAlive();
 
 
-const myMonitor = new Monitor({
-  website: process.env.PING_TARGET,
-  title: process.env.PING_TARGET,
-  interval: 2
-});
+// const myMonitor = new Monitor({
+//   website: process.env.PING_TARGET,
+//   title: process.env.PING_TARGET,
+//   interval: 2
+// });
 
 
 
-myMonitor.on('up', function (res, state) {
-    console.log('Yay!! ' + state.address + ' is up.');
-});
-myMonitor.on('down', function (res, state) {
-  console.log('Oh Snap!! ' + state.address + ' is down! ' + state.statusMessage);
-});
-myMonitor.on('restored', function (res, state) {
-  console.log(state.address + ' has been restore');
-});
-myMonitor.on('stop', function (res, state) {
-  console.log(state.address + ' monitor has stopped.');
-});
-myMonitor.on('timeout', function (error, res) {
-  console.log(error);
-});
-myMonitor.on('error', function (error) {
-  console.log(error);
-});
+// myMonitor.on('up', function (res, state) {
+//     console.log('Yay!! ' + state.address + ' is up.');
+// });
+// myMonitor.on('down', function (res, state) {
+//   console.log('Oh Snap!! ' + state.address + ' is down! ' + state.statusMessage);
+// });
+// myMonitor.on('restored', function (res, state) {
+//   console.log(state.address + ' has been restore');
+// });
+// myMonitor.on('stop', function (res, state) {
+//   console.log(state.address + ' monitor has stopped.');
+// });
+// myMonitor.on('timeout', function (error, res) {
+//   console.log(error);
+// });
+// myMonitor.on('error', function (error) {
+//   console.log(error);
+// });
